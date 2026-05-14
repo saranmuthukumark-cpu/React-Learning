7967be8b00a70c1b051482fcda3b65f1430d9aae
saranmuthukumark-cpu <saranmuthukumar.k@gmail.com>
2026-05-14 18:22:24 +0530

    fix

--- day6-router/src/App.jsx
+++ day6-router/src/App.jsx
@@ -1,10 +1,6 @@
 import "./App.css";
 import React from "react";
-<<<<<<< HEAD
 import Blog from "./components/blog/Blog";
-=======
-import {Blog} from "./components/blog/Blog";
->>>>>>> 7873d17771031bfa013f44a93b869fd2b211226e
 import Hero from "./components/Hero";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/login/Login";
